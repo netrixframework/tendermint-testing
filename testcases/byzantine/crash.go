0473fb8a891114ea17371f6e3b54ae5330d3f6b9
Srinidhi N <srinidhin@outlook.com>
2022-05-09 10:00:27 +0200

    Adding the test files

---
+++ testcases/byzantine/crash.go
@@ -0,0 +1,59 @@
+package byzantine
+
+import (
+	"time"
+
+	"github.com/netrixframework/netrix/testlib"
+	"github.com/netrixframework/tendermint-testing/common"
+)
+
+func CrashReplica(sp *common.SystemParams) *testlib.TestCase {
+	sm := testlib.NewStateMachine()
+	init := sm.Builder()
+	roundOne := init.On(
+		common.RoundReached(1),
+		"roundOne",
+	)
+	roundOne.On(
+		common.IsCommit(),
+		testlib.SuccessStateLabel,
+	)
+
+	filters := testlib.NewFilterSet()
+	filters.AddFilter(common.TrackRoundTwoThirds)
+	// Need to figure out a way around this
+	// filters.AddFilter(
+	// 	testlib.If(
+	// 		testlib.Once(sm.InState("roundOne")),
+	// 	).Then(
+	// 		testlib.StopReplica(common.RandomReplicaFromPart("faulty")),
+	// 	),
+	// )
+	filters.AddFilter(
+		testlib.If(
+			testlib.IsMessageSend().
+				And(common.IsMessageFromRound(0)).
+				And(common.IsVoteFromFaulty()),
+		).Then(
+			common.ChangeVoteToNil(),
+		),
+	)
+	filters.AddFilter(
+		testlib.If(
+			testlib.IsMessageSend().
+				And(common.IsMessageFromRound(0)).
+				And(common.IsVoteFromPart("h")),
+		).Then(
+			testlib.DropMessage(),
+		),
+	)
+
+	testcase := testlib.NewTestCase(
+		"CrashReplica",
+		2*time.Minute,
+		sm,
+		filters,
+	)
+	testcase.SetupFunc(common.Setup(sp))
+	return testcase
+}
